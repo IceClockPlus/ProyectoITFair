7f66f45138003388c30efc3ec338659a1efdde74
Mirko <Mirkoivanovich@yahoo.es>
2018-11-12 11:56:17 -0300

    Commit temporal para seguir trabajando

--- ITFair/Fuente_Pagina/urls.py
+++ ITFair/Fuente_Pagina/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('clase2', views.clase2, name='clase2'),
-    path('clase/<int:id>', views.clase, name="clase")
+    path('clase/<int:id>', views.clase, name="clase"),
+    path('nueva_clase',views.nueva_clase,name="nueva_clase")
 ]
--- ITFair/Fuente_Pagina/views.py
+++ ITFair/Fuente_Pagina/views.py
@@ -12,3 +12,10 @@ def clase(request, id):
     lec = Leccion.objects.get(pk = id)
     items = ItemLeccion.objects.filter(leccion = lec)
     return render(request,'clases.html',{'leccion':lec,'items':items})
+
+def nueva_clase(request)
+    nombre = request.POST.get['nombre']
+    imgInp = request.POST.get['imgInp']
+    descripcion = request.POST.get['descripcion']
+    ylink = request.POST.get['ylink']
+    lec = Leccion(nombre=nombre,resumen=descripcion,imagen=imgInp,video=ylink)
